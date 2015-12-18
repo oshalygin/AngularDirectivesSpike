af7ca14627ee3e445b7dfc45ee2937e1e7e296c3
Oleg Shalygin <oshalygin@gmail.com>
2015-12-17 21:34:34 -0800

    tying together a bit of node to run locally

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-var http = require("http");
-http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write("Derp");
-    response.end();
+var express = require("express");
+var path = require("path");
 
-}).listen(9999);
+var application = express();
+
+application.get("/", function (request, response) {
+    response.sendFile(path.join(__dirname + "/wwwroot/index.html"));
+});
+
+application.use(express.static("wwwroot"));
+application.use(express.static("node_modules"));
+application.use(express.static("bower_components"));
+
+var server = application.listen(9999, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Listening on http://%s:%s", host, port);
+
+});
